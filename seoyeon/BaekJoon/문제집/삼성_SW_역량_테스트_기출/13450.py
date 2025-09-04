bb175df0752cd322dba8fda613d759c22a702b15
seoyeon <psy0201123@gmail.com>
2025-09-04 23:51:08 +0900

    [BOJ] 13460 구슬탈출2-DFS. 테케성공+틀렸습니다

--- seoyeon/BaekJoon/문제집/삼성_SW_역량_테스트_기출/13450.py
+++ seoyeon/BaekJoon/문제집/삼성_SW_역량_테스트_기출/13450.py
@@ -1,5 +1,5 @@
 #백준 #13460 구슬탈출2
-#구현
+#구현, dfs
 #20:30-21:50
 
 
@@ -15,8 +15,6 @@ for i in range(N):
             rx,ry = i,j
         elif boards[i][j]=="B":
             bx,by = i,j
-        elif boards[i][j]=="O":
-            hx,hy = i,j
 
 #이동 동작
 #왼쪽,오른쪽,위,아래로 기울이기
@@ -27,248 +25,59 @@ for i in range(N):
 # 1)빨간 구슬 좌표와 구멍의 좌표 비교
 # 2)위치에 따라 상하좌우로 이동
 
-answer = -1
-#print("rx,ry,hx,hy",rx,ry,hx,hy)
-def up(rx,ry,bx,by,hx,hy):
-    result = 0 #0이면 이동은 가능, 1이면 아예 종료, -1이면 아예 이동 불가능
+answer = 11
+
+dir = [[0,1],[0,-1],[1,0],[-1,0]]
+
+def move(x,y,d):
+    cnt = 0
+
+    #현재 위치가 구멍이 아니고, 다음이 벽도 아니면 이동
+    #즉, 현재 위치가 구멍이거나 다음이 벽이면 이동하지 않음
+    while boards[x+dir[d][0]][y+dir[d][1]]!="#" and boards[x][y]!="O":
+        x += dir[d][0]
+        y += dir[d][1]
+        cnt += 1
     
-    #rx,bx 비교해 더 작은 값 먼저 이동(위로 이동하므로)
-    #1)빨간구슬 먼저 이동
-    if rx<bx:
-        #1-1)빨간구슬 이동
-        while True:
-            rx -= 1
-            if rx==hx and ry==hy:
-                result = 1
-            if rx<0 or boards[rx][ry]!=".":
-                break
-            
-        #1-2)파란구슬 이동
-        while True:
-            bx -= 1
-            if bx==hx and by==hy:
-                result = -1
-            if bx<0 or boards[bx][by]!=".":
-                break
-            
+    return [cnt,x,y]
 
-    #2)파란구슬 먼저 이동
-    else:
-        #2-1)파란구슬 이동
-        while True:
-            bx -= 1
-            if bx==hx and by==hy:
-                result = -1
-            if bx<0 or boards[bx][by]!=".":
-                break
-            
 
-        #2-2)빨간구슬 이동
-        if result!=-1:
-            while True:
-                rx -= 1
-                if rx==hx and ry==hy:
-                    result = 1
-                if rx<0 or boards[rx][ry]!=".":
-                    break
-                
-
-    return [result,rx,ry,bx,by]
-
-def down(rx,ry,bx,by,hx,hy):
-    result = 0 #0이면 이동은 가능, 1이면 아예 종료, -1이면 아예 이동 불가능
-    
-    #rx,bx 비교해 더 작은 값 먼저 이동(위로 이동하므로)
-    #1)빨간구슬 먼저 이동
-    if rx>bx:
-        #1-1)빨간구슬 이동
-        while True:
-            rx += 1
-            if rx==hx and ry==hy:
-                result = 1
-            if rx>N or boards[rx][ry]!=".":
-                break
-            
-        #1-2)파란구슬 이동
-        while True:
-            bx += 1
-            if bx==hx and by==hy:
-                result = -1
-            if bx>N or boards[bx][by]!=".":
-                break
-            
-
-    #2)파란구슬 먼저 이동
-    else:
-        #2-1)파란구슬 이동
-        while True:
-            bx += 1
-            if bx==hx and by==hy:
-                result = -1
-            if bx>N or boards[bx][by]!=".":
-                break
-            
-
-        #2-2)빨간구슬 이동
-        if result!=-1:
-            while True:
-                rx += 1
-                if rx==hx and ry==hy:
-                    result = 1
-                if rx>N or boards[rx][ry]!=".":
-                    break
-                
-
-    return [result,rx,ry,bx,by]
-
-def left(rx,ry,bx,by,hx,hy):
-    result = 0 #0이면 이동은 가능, 1이면 아예 종료, -1이면 아예 이동 불가능
-    
-    #rx,bx 비교해 더 작은 값 먼저 이동(위로 이동하므로)
-    #1)빨간구슬 먼저 이동
-    if ry<by:
-        #1-1)빨간구슬 이동
-        while True:
-            ry -= 1
-            if rx==hx and ry==hy:
-                result = 1
-            if ry<0 or boards[rx][ry]!=".":
-                break
-            
-        #1-2)파란구슬 이동
-        while True:
-            by -= 1
-            if bx==hx and by==hy:
-                result = -1
-            if by<0 or boards[bx][by]!=".":
-                break
-            
-
-    #2)파란구슬 먼저 이동
-    else:
-        #2-1)파란구슬 이동
-        while True:
-            by -= 1
-            #print("blue",by)
-            if bx==hx and by==hy:
-                result = -1
-            if by<0 or boards[bx][by]!=".":
-                break
-
-        #2-2)빨간구슬 이동
-        if result!=-1:
-            #print("ddddddd")
-            while True:
-                ry -= 1
-                if rx==hx and ry==hy:
-                    result = 1
-                if ry<0 or boards[rx][ry]!=".":
-                    break
-
-    #print("result",result,rx,ry,bx,by)
-    return [result,rx,ry,bx,by]
-
-def right(rx,ry,bx,by,hx,hy):
-
-    result = 0 #0이면 이동은 가능, 1이면 아예 종료, -1이면 아예 이동 불가능
-    
-    #rx,bx 비교해 더 작은 값 먼저 이동(위로 이동하므로)
-    #1)빨간구슬 먼저 이동
-    if ry>by:
-        #1-1)빨간구슬 이동
-        while True:
-            ry += 1
-            if rx==hx and ry==hy:
-                result = 1
-            if ry>N or boards[rx][ry]!=".":
-                break
-        #1-2)파란구슬 이동
-        while True:
-            by += 1
-            if bx==hx and by==hy:
-                result = -1
-            if by>N or boards[bx][by]!=".":
-                break
-
-    #2)파란구슬 먼저 이동
-    else:
-        #2-1)파란구슬 이동
-        while True:
-            by += 1
-            if bx==hx and by==hy:
-                result = -1
-            if by>N or boards[bx][by]!=".":
-                break
-
-        #2-2)빨간구슬 이동
-        if result!=-1:
-            #print("HHHHHHHHHHHDJFHDJFJD")
-            while True:
-                ry += 1
-                #print("INSIDE rxry",rx,ry,"hyhy",hx,hy)
-                if rx==hx and ry==hy:
-                    #print("WOWOWOOWOWOWO")
-                    result = 1
-                if ry>N or boards[rx][ry]!=".":
-                    break
-
-    return [result,rx,ry,bx,by]
-
-def dfs(rx,ry,bx,by,hx,hy,cnt):
+def dfs(rx,ry,bx,by,cnt):
     global answer
-
-    if cnt==2:
+    #print("rxry",rx,ry,"bxby",bx,by,"cnt",cnt)
+    if cnt>10:
         return
+    
+    rrcnt,bbcnt = 0,0
 
-    #위치에 따라 구멍좌표 -> 빨간구슬 좌표로 이동
-    #위로 이동
-    if rx>hx:
-        #print("up","cnt",cnt,"rx,ry",rx,ry)
-        lst = up(rx,ry,bx,by,hx,hy)
-        #빨간색만 들어간 경우
-        if lst[0]==1:
-            answer = cnt
+    #상하좌우로 이동
+    for d in range(4):
+        rrcnt, rrx, rry = move(rx,ry,d)
+        bbcnt, bbx, bby = move(bx,by,d)
+        #1.파란 구슬이 구멍에 들어가면 실패
+        if boards[bbx][bby]=="O":
+            #print("here11")
             return
-        #파란색에는 안 들어간 경우, 다음 이어가도 됨
-        elif lst[0]==0:
-            rxx,ryy, bxx,byy = lst[1],lst[2],lst[3],lst[4]
-            dfs(rxx,ryy,bxx,byy,hx,hy,cnt+1)
-    #아래로 이동
-    if rx<hx:
-        #print("down","cnt",cnt,"rx,ry",rx,ry)
-        lst = down(rx,ry,bx,by,hx,hy)
-        #빨간색만 들어간 경우
-        if lst[0]==1:
-            answer = cnt
+        #2.빨간 구슬이 구멍에 들어가면 성공
+        if boards[rrx][rry]=="O":
+            #print("here22")
+            answer = min(answer,cnt)
             return
-        #파란색에는 안 들어간 경우, 다음 이어가도 됨
-        elif lst[0]==0:
-            rxx,ryy, bxx,byy = lst[1],lst[2],lst[3],lst[4]
-            dfs(rxx,ryy,bxx,byy,hx,hy,cnt+1)
-    #왼쪽으로 이동
-    if ry>hy:
-        #print("left","cnt",cnt,"rx,ry",rx,ry)
-        lst = left(rx,ry,bx,by,hx,hy)
-        #빨간색만 들어간 경우
-        if lst[0]==1:
-            answer = cnt
-            return
-        #파란색에는 안 들어간 경우, 다음 이어가도 됨
-        elif lst[0]==0:
-            rxx,ryy, bxx,byy = lst[1],lst[2],lst[3],lst[4]
-            dfs(rxx,ryy,bxx,byy,hx,hy,cnt+1)
-    #오른쪽으로 이동
-    if ry<hy:
-        #print("right","cnt",cnt,"rx,ry",rx,ry)
-        lst = right(rx,ry,bx,by,hx,hy)
-        #빨간색만 들어간 경우
-        if lst[0]==1:
-            answer = cnt
-            return
-        #파란색에는 안 들어간 경우, 다음 이어가도 됨
-        elif lst[0]==0:
-            rxx,ryy, bxx,byy = lst[1],lst[2],lst[3],lst[4]
-            dfs(rxx,ryy,bxx,byy,hx,hy,cnt+1)
 
-dfs(rx,ry,bx,by,hx,hy,0)
-print(answer+1)
+        #겹치는 경우 멀리서 온 구슬 재배치
+        if rrx==bbx and rry==bby:
+            if rrcnt>bbcnt:
+                rrx -= dir[d][0]
+                rry -= dir[d][1]
+            elif rrcnt<bbcnt:
+                bbx -= dir[d][0]
+                bby -= dir[d][1]
+        
+        dfs(rrx,rry,bbx,bby,cnt+1)
+
+dfs(rx,ry,bx,by,1)
+
+if answer==11:
+    print(-1)
+else:
+    print(answer)
