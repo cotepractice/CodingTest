2f2cdddcdb2a079b99ec3074e9fdccd73b068160
seoyeon <psy0201123@gmail.com>
2025-09-09 00:30:31 +0900

    [BOJ] 14500 테트로미노-직접 회전+대칭

--- seoyeon/BaekJoon/Folder/Samsung_SW_Test/14500.py
+++ seoyeon/BaekJoon/Folder/Samsung_SW_Test/14500.py
@@ -2,70 +2,70 @@
 # 2:40
 # "ㅗ" 제외한 블럭이 블럭 4개로 갈 수 있는 모든 경로!
 
-#2. ㅗ 제외 DFS로 탐색
+# #2. ㅗ 제외 DFS로 탐색
 
-N,M = map(int,input().split()) #N:세로, M:가로 (4<=N,M<=500)
-boards = [[0 for _ in range(M)] for _ in range(N)]
+# N,M = map(int,input().split()) #N:세로, M:가로 (4<=N,M<=500)
+# boards = [[0 for _ in range(M)] for _ in range(N)]
 
-for i in range(N):
-    board = list(map(int,input().split()))
-    boards[i]=board
+# for i in range(N):
+#     board = list(map(int,input().split()))
+#     boards[i]=board
 
-visited=[[False for _ in range(M)] for _ in range(N)]
-maximum = 0
-#print("visited",visited)
+# visited=[[False for _ in range(M)] for _ in range(N)]
+# maximum = 0
+# #print("visited",visited)
 
-dir = [[1,0],[-1,0],[0,-1],[0,1]]
+# dir = [[1,0],[-1,0],[0,-1],[0,1]]
 
-# "ㅗ" 제외
-def dfs(x,y,tmp,cnt):
-    global maximum
+# # "ㅗ" 제외
+# def dfs(x,y,tmp,cnt):
+#     global maximum
 
-    if cnt==4:
-        maximum = max(maximum,tmp)
-        return
+#     if cnt==4:
+#         maximum = max(maximum,tmp)
+#         return
     
-    for dx,dy in dir:
-        nx = x+dx
-        ny = y+dy
-        #print("nx",nx,ny)
-        if 0<=nx<N and 0<=ny<M and visited[nx][ny]==False:
-            visited[nx][ny]=True
-            dfs(nx,ny,tmp+boards[nx][ny],cnt+1)
-            visited[nx][ny]=False
+#     for dx,dy in dir:
+#         nx = x+dx
+#         ny = y+dy
+#         #print("nx",nx,ny)
+#         if 0<=nx<N and 0<=ny<M and visited[nx][ny]==False:
+#             visited[nx][ny]=True
+#             dfs(nx,ny,tmp+boards[nx][ny],cnt+1)
+#             visited[nx][ny]=False
 
-# "ㅗ" 모양. [x,y]를 기준으로 상하좌우 탐색해 가장 큰 값으로 만듬 
-def fy(x,y):
-    global maximum
+# # "ㅗ" 모양. [x,y]를 기준으로 상하좌우 탐색해 가장 큰 값으로 만듬 
+# def fy(x,y):
+#     global maximum
 
-    arr = []
+#     arr = []
 
-    for dx,dy in dir:
-        nx = x+dx
-        ny = y+dy
-        if 0<=nx<N and 0<=ny<M:
-            arr.append(boards[nx][ny])            
+#     for dx,dy in dir:
+#         nx = x+dx
+#         ny = y+dy
+#         if 0<=nx<N and 0<=ny<M:
+#             arr.append(boards[nx][ny])            
 
-    #4개면 가장 작은 값 제거
-    if len(arr)==4:
-        arr.sort(reverse=True)
-        arr.pop()
-        maximum = max(maximum, sum(arr)+boards[x][y])
+#     #4개면 가장 작은 값 제거
+#     if len(arr)==4:
+#         arr.sort(reverse=True)
+#         arr.pop()
+#         maximum = max(maximum, sum(arr)+boards[x][y])
     
-    elif len(arr)==3:
-        maximum = max(maximum, sum(arr)+boards[x][y])
-    return 
+#     elif len(arr)==3:
+#         maximum = max(maximum, sum(arr)+boards[x][y])
+#     return 
 
 
-#Bruteforce
-for i in range(N):
-    for j in range(M):
-        visited[i][j]=True
-        dfs(i,j,boards[i][j],1)
-        fy(i,j)
-        visited[i][j]=False
+# #Bruteforce
+# for i in range(N):
+#     for j in range(M):
+#         visited[i][j]=True
+#         dfs(i,j,boards[i][j],1)
+#         fy(i,j)
+#         visited[i][j]=False
 
-print(maximum)
+# print(maximum)
 
 
 # from collections import deque
@@ -190,3 +190,77 @@ print(maximum)
 #                 answer = max(answer,ans)
         
 # print(answer)            
+
+# # 백준 14500 테트로미노
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+boards = [list(map(int, input().split())) for _ in range(N)]
+
+# 기본 블럭 5개 (회전/대칭 전)
+# 상대좌표 (0,0) 기준
+base = [
+    [[0,0],[0,1],[0,2],[0,3]],   # ㅡ
+    [[0,0],[1,0],[0,1],[1,1]],   # ㅁ
+    [[0,0],[1,0],[2,0],[2,1]],   # ㄴ
+    [[0,0],[1,0],[1,1],[2,1]],   # ㄹ
+    [[0,0],[0,1],[0,2],[1,1]]    # ㅗ
+]
+
+def normalize(block):
+    min_x = min(x for x,y in block)
+    min_y = min(y for x,y in block)
+    #좌표들을 (0,0) 기준으로 이동 후 정렬
+    norm = sorted([[x-min_x, y-min_y] for x,y in block])
+
+    return tuple(map(tuple, norm))   # set에 넣기 위해 tuple로 변환
+
+#왼쪽으로 90도 회전
+def rotate(block):
+    return [[y, -x] for x,y in block]
+
+#y축 대칭
+def symmetry(block):
+    return [[x, -y] for x,y in block]
+
+# 모든 블럭 좌표 구하기
+all_tetromino = set()
+
+for b in base:
+    shapes = set()
+    q = [b]
+
+    while q:
+        cur = q.pop()
+        norm = normalize(cur)
+        if norm in shapes:
+            continue
+        shapes.add(norm)
+
+        # 회전
+        q.append(rotate(cur))
+        # 대칭
+        q.append(symmetry(cur))
+
+    # shapes 안에 이 블럭에서 만들 수 있는 모든 변형이 들어있음
+    all_tetromino.update(shapes)
+
+# 이제 모든 보드 위치에서 블럭 올려보기
+answer = 0
+for i in range(N):
+    for j in range(M):
+        for block in all_tetromino:
+            s = 0
+            ok = True
+            for dx, dy in block:
+                x, y = i+dx, j+dy
+                if 0 <= x < N and 0 <= y < M:
+                    s += boards[x][y]
+                else:
+                    ok = False
+                    break
+            if ok:
+                answer = max(answer, s)
+
+print(answer)
