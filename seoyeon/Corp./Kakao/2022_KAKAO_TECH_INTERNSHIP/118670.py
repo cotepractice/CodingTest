00eb96153861e88b3dd23c694c3186acba26137b
seoyeon <psy0201123@gmail.com>
2025-10-09 21:01:11 +0900

    [PSG] 2022 KAKAO TECH INTERNSHIP-118670

--- seoyeon/Corp./Kakao/2022_KAKAO_TECH_INTERNSHIP/118670.py
+++ seoyeon/Corp./Kakao/2022_KAKAO_TECH_INTERNSHIP/118670.py
@@ -1,4 +1,4 @@
-#15:30-
+#1. 시간 초과
 import copy
 from collections import deque
 
@@ -68,3 +68,67 @@ def solution(rc, operations):
             rc=shiftrow(rc)
     
     return rc
+
+#2. 시간초과 해결
+#15:30-
+import copy
+from collections import deque,defaultdict
+
+N,M = 0,0
+
+def solution(rc, operations):
+    global N,M
+    answer = []
+    
+    N = len(rc)
+    M = len(rc[0])
+    
+    #rows는 rc[n][1:M-1] 가장 앞 원소와 마지막 원소 제외
+    #제외된 원소는 right_cols, left_cols에 포함
+    rows = deque(deque(rc[i][1:M-1]) for i in range(N))
+    right_cols = deque(rc[i][M-1] for i in range(N))
+    left_cols = deque(rc[i][0] for i in range(N-1,-1,-1))
+    
+    for o in operations:
+        #연산1:가장자리 회전
+        if o=="Rotate":
+            # print("Rotate B")
+            # print(rows)
+            # print(right_cols)
+            # print(left_cols)
+            
+            rows[0].appendleft(left_cols.pop()) #left_cols 마지막 원소 빼서 rows[0]에
+            right_cols.appendleft(rows[0].pop()) #rows[0] 마지막 원소 빼서 right_cols에
+            rows[N-1].append(right_cols.pop()) #right_cols 마지막 원소 빼서 rows[N-1]에
+            left_cols.appendleft(rows[N-1].popleft()) #rows[N-1] 마지막 원소 빼서 left_cols에 
+
+            # print("Rotate A")
+            # print(rows)
+            # print(right_cols)
+            # print(left_cols)
+        #연산2:마지막 행을 맨 위로 
+        #1.rows: 맨 뒤 원소를 빼서 앞에 넣기
+        #2.right_cols, left_cols 
+        if o=="ShiftRow":
+            # print("ShiftRow B")
+            # print(rows)
+            # print(right_cols)
+            # print(left_cols)
+            
+            row_lst = rows.pop()
+            rows.appendleft(row_lst)
+            right_cols.appendleft(right_cols.pop())
+            left_cols.append(left_cols.popleft())
+            # print("ShiftRow A")
+            # print(rows)
+            # print(right_cols)
+            # print(left_cols)
+    
+    for k in range(N):
+        ans=[]
+        ans.append(left_cols.pop())
+        ans.extend(rows[k])
+        ans.append(right_cols.popleft())
+        answer.append(ans)
+    
+    return answer
